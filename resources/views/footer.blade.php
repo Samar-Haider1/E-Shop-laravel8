c3494d6af44f18bbbfd7d76823d2d6076b7f56fa
samar <sammarrizvi05@gmail.com>
2020-11-11 04:39:42 -0800

    first laravel8 project Header,Footer and Login page Done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,18 @@
+<div class="card text-white bg-dark" >
+    <div class="card-body">
+        <blockquote class="blockquote mb-0">
+            <p>
+                “We see our customers as invited guests to a party, and we are the hosts.
+                It’s our job every day to make every important aspect of the customer experience a little bit better.”
+            </p>
+        </blockquote>
+    </div>
+</div>
+{{--<div class="media position-relative">--}}
+{{--    <img src="" class="mr-3" alt="...">--}}
+{{--    <div class="media-body">--}}
+{{--        <h5 class="mt-0">Media with stretched link</h5>--}}
+{{--        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>--}}
+{{--        <a href="#" class="stretched-link">Home</a>--}}
+{{--    </div>--}}
+{{--</div>--}}
